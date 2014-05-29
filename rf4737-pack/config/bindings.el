b052af29ec26111a22ddc46afffb4d69954d490c
Robert Farr <rfarr@cerner.com>
2014-05-29 15:44:19 -0500

    Make iterm2 and emacs play together.

--- rf4737-pack/config/bindings.el
+++ rf4737-pack/config/bindings.el
@@ -6,6 +6,24 @@
 (defadvice terminal-init-xterm (after select-shift-up activate)
     (define-key input-decode-map "\e[1;2A" [S-up]))
 
+(define-key input-decode-map "\e[1;6A" [C-S-up])
+(define-key input-decode-map "\e[1;6B" [C-S-down])
+(define-key input-decode-map "\e[1;6C" [C-S-right])
+(define-key input-decode-map "\e[1;6D" [C-S-left])
+(define-key input-decode-map "\e[1;8A" [C-M-up])
+(define-key input-decode-map "\e[1;8B" [C-M-down])
+(define-key input-decode-map "\e[1;8C" [C-M-right])
+(define-key input-decode-map "\e[1;8D" [C-M-left])
+(define-key input-decode-map "\e[1;9A" [M-up])
+(define-key input-decode-map "\e[1;9B" [M-down])
+(define-key input-decode-map "\e[1;9C" [M-right])
+(define-key input-decode-map "\e[1;9D" [M-left])
+(define-key input-decode-map "\e[1;10A" [S-M-up])
+(define-key input-decode-map "\e[1;10B" [S-M-down])
+(define-key input-decode-map "\e[1;10C" [S-M-right])
+(define-key input-decode-map "\e[1;10D" [S-M-left])
+
+
 ;; For example:
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
