c3326bf075a0b96484443e568538777588af02c6
Farr Jr <rf4737@mac-rf4737.northamerica.cerner.net>
2014-05-29 14:56:54 -0500

    config wind-move

--- rf4737-pack/config/bindings.el
+++ rf4737-pack/config/bindings.el
@@ -1,5 +1,11 @@
 ;; Place your bindings here.
 
+; activate windmove
+(when (fboundp 'windmove-default-keybindings)
+      (windmove-default-keybindings))
+(defadvice terminal-init-xterm (after select-shift-up activate)
+    (define-key input-decode-map "\e[1;2A" [S-up]))
+
 ;; For example:
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
