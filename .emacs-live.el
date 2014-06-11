064524a928eb5179660a9dc735db3be80fa5a75b
Robert Farr <rfarr@cerner.com>
2014-06-10 23:07:12 -0500

    Custom .emacs-live.el file

---
+++ .emacs-live.el
@@ -0,0 +1,9 @@
+(live-use-packs '(stable/foundation-pack stable/power-pack stable/lang-pack stable/git-pack stable/org-pack stable/clojure-pack))
+
+;; Zenburn pack
+(live-add-packs '(~/.live-packs/zenburn-pack))
+
+;; Solarized pack
+;;(live-add-packs '(~/.live-packs/solarized-pack))
+
+(live-add-packs '(~/.live-packs/rf4737-pack))
